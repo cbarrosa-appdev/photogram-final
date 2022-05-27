2e5ee6e1e6fb76737e65d6ec220d5c2e5f706e5f
cbarrosa <cbarrosa@chicagobooth.edu>
2022-05-27 03:16:13 +0000

    add foto y fotos privadas

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,6 +28,7 @@ class PhotosController < ApplicationController
     if the_photo.valid?
       the_photo.save
       redirect_to("/photos", { :notice => "Photo created successfully." })
+      
     else
       redirect_to("/photos", { :alert => the_photo.errors.full_messages.to_sentence })
     end
