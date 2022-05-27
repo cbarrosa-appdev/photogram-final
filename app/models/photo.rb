8bdeda8d8ce665492aa4b8b1c3b4f03aa4575b89
cbarrosa <cbarrosa@chicagobooth.edu>
2022-05-27 03:03:27 +0000

    realacion foto usuario

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,4 +12,5 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+  belongs_to(:owner, {:class_name=>"User", :foreign_key=>"owner_id"})
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,5 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+  has_many(:photos, {:class_name=>"Photo",:foreign_key=>"owner_id"})
 end
